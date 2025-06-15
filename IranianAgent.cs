bce2c33405aba5cf2dbd9b07a48b51b84beba2a7
elazar matan <elazarmata8@gmail.com>
2025-06-15 11:37:26 +0300

    Add abstract class IranianAgent with constructor
    Introduced the `IranianAgent` abstract class featuring protected fields for `name`, `rank`, `weaknes`, and `empty`. Implemented a constructor to initialize `name` and `rank`.

---
+++ IranianAgent.cs
@@ -0,0 +1,13 @@
+﻿public abstract class IranianAgent
+{
+    protected string name;
+    protected string rank;
+    protected Sensor[] weaknes;
+    protected Sensor[] empty;
+
+    public IranianAgent(string name,string rank)
+    {
+        this.name = name;
+        this.rank = rank;
+    }
+}
