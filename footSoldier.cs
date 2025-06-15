969a65768ea0d8e4797123000adaa54859a1b00a
elazar matan <elazarmata8@gmail.com>
2025-06-15 11:37:18 +0300

    Add IranianAgent class and update project references
    Updated `SENSORS.csproj` to include `footSoldier.cs` and `IranianAgent.cs` as compile items. Introduced a new abstract class `IranianAgent` with protected fields and a constructor for initializing `name` and `rank`.

---
+++ footSoldier.cs
@@ -0,0 +1,8 @@
+﻿public class footSoldier: IranianAgent
+{
+    public footSoldier(string name) : base(name,"foot soldier") 
+    {
+        this.weaknes = new Sensor[2];
+        this.empty = new Sensor[2];
+    }
+}
