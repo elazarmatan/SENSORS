a7fc712f4f377784e4988d18bbf00dadbd320f86
elazar matan <elazarmata8@gmail.com>
2025-06-16 12:02:30 +0300

    Update footSoldier class constructor and imports
    Modified the footSoldier class to include a new constructor that accepts a List<Sensor> for initializing the weaknes field with random sensors. Removed the old constructor that only took a string name. Added necessary using directives for System and System.Collections.Generic.

--- footSoldier.cs
+++ footSoldier.cs
@@ -1,8 +1,14 @@
-﻿public class footSoldier: IranianAgent
+﻿using System;
+using System.Collections.Generic;
+
+public class footSoldier: IranianAgent
 {
-    public footSoldier(string name) : base(name,"foot soldier") 
+    public footSoldier(string name, List<Sensor> sensors) : base(name,"foot soldier") 
     {
-        this.weaknes = new Sensor[2];
+        Random rand = new Random();
+        int weak1 = rand.Next(sensors.Count);
+        int weak2 = rand.Next(sensors.Count);
+        this.weaknes = new Sensor[] { sensors[weak1], sensors[weak2] };
         this.empty = new Sensor[2];
     }
 }
