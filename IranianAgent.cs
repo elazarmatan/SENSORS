8330478ed273bc057a790b1f98bba4e7a6331b00
elazar matan <elazarmata8@gmail.com>
2025-06-17 09:26:31 +0300

    Refactor IranianAgent class and add createWeaknes method
    - Added using directives for System and System.Collections.Generic.
    - Changed 'name' from protected to public for external access.
    - Updated 'weaknes' from Sensor array to public List<string>.
    - Introduced protected property 'Pinned' as a List<Sensor>.
    - Added 'createWeaknes' method to populate 'weaknes' list randomly.

--- IranianAgent.cs
+++ IranianAgent.cs
@@ -1,13 +1,34 @@
-﻿public abstract class IranianAgent
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class IranianAgent
 {
-    protected string name;
+    public string name;
     protected string rank;
-    protected Sensor[] weaknes;
-    protected Sensor[] empty;
+    
+    public List<string> weaknes = new List<string>();
+    protected List<Sensor> Pinned { get; set; }
 
     public IranianAgent(string name,string rank)
     {
         this.name = name;
         this.rank = rank;
+        
+    }
+
+    
+
+    
+
+    public List<string> createWeaknes(List<string> sensors,int range)
+    {
+        
+        Random rand = new Random();
+        for(int i = 0;i < range;i++)
+        {
+            int weak1 = rand.Next(sensors.Count);
+            this.weaknes.Add(sensors[weak1]);
+        }
+        return this.weaknes;
     }
 }
