ef7130d56af719dc6ad0377930ee87f4c245dec8
elazar matan <elazarmata8@gmail.com>
2025-06-17 15:09:00 +0300

    Add game method to GameManager for audio processing
    This commit introduces a new static method `game` in the `GameManager` class. It initializes an `Audio` instance and sets up lists for sensor names and sensors. The method retrieves an `IranianAgent` using the `investRank1` method from the `Investigation` class and checks if the agent is contained within the investigation.

--- gameManager.cs
+++ gameManager.cs
@@ -1,6 +1,15 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 public static class GameManager
 {
-    
+    public static void game()
+    {
+        Audio a = new Audio();
+        List<string> sensorname = new List<string>() { "audio" };
+        List<Sensor> sensors = new List<Sensor>() { a };
+        IranianAgent age = Investigation.investRank1(sensorname, sensors);
+        age.getSensor(sensors, age.id);
+        Investigation.contains(age);
+    }
 }
