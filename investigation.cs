ddd58158a2e304844eb9bed50d2d192eb1dd616c
elazar matan <elazarmata8@gmail.com>
2025-06-17 15:09:11 +0300

    Refactor Investigation methods for clarity and efficiency
    Updated `investRank1` to use agent's `id` for weakness generation.
    Improved console output formatting in `contains` method by removing unnecessary spaces and debug statements.
    This enhances readability and streamlines the code.

--- investigation.cs
+++ investigation.cs
@@ -7,9 +7,8 @@ public static  class Investigation
     public static IranianAgent investRank1(List<string> sensorname, List<Sensor> sensors)
     {
         IranianAgent age = new agentRank1("faruk");
-        List<string> weaknes = age.createWeaknes(sensorname,2);
-        
-        Dictionary<string, int> weaknesDict = age.convertListToDictionary(weaknes);
+        List<string> weaknes = age.createWeaknes(sensorname,age.id);
+        age.f();
         return age;
     }
 
@@ -19,22 +18,21 @@ public static  class Investigation
 
     public static bool contains(IranianAgent agent)
     {
+       
+
         int required = agent.id;
         int count = 0;
         foreach (Sensor s in agent.Pinned) 
         {
             if (agent.weaknesDict.ContainsKey(s.Activate()))
             {
-                Console.WriteLine("a");
                 if (agent.weaknesDict[s.Activate()] != 0)
                 {
-                    Console.WriteLine("b");
                     agent.weaknesDict[s.Activate()] --;
                     count++;
-                    Console.WriteLine($"succes {count} / {required}");
+                    Console.WriteLine($"succes {count}/{required}");
                     if(count == required)
                     {
-                        Console.WriteLine("c");
                         return true;
                         
                     }
