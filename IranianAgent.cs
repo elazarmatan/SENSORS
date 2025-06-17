be17b2d3980811df759cbeb2d029be512b207e0d
elazar matan <elazarmata8@gmail.com>
2025-06-17 12:36:19 +0300

    Enhance IranianAgent with new properties and methods
    Updated the IranianAgent class to include an integer `id` property and a `weaknesDict` for storing weaknesses. The `weaknes` list is now a public property, and the `Pinned` list is initialized directly. The constructor has been modified to accept the new `id` parameter. New methods added include `getSensor`, `pinnedSensor`, `convertListToDictionary`, and `printList` for improved sensor management and functionality.

--- IranianAgent.cs
+++ IranianAgent.cs
@@ -5,20 +5,65 @@ public abstract class IranianAgent
 {
     public string name;
     protected string rank;
-    
+    public int id;
     public List<string> weaknes = new List<string>();
-    protected List<Sensor> Pinned { get; set; }
+    public List<Sensor> Pinned = new List<Sensor>();
+    public Dictionary<string, int> weaknesDict;
 
-    public IranianAgent(string name,string rank)
+    public IranianAgent(string name,string rank,int id)
     {
         this.name = name;
         this.rank = rank;
-        
+        this.id = id;
     }
 
-    
+    public List<Sensor> getSensor(List<Sensor> sensors, int range)
+    {
+        for (int i = 0; i < sensors.Count; i++)
+        {
+            Console.WriteLine($"{sensors[i].Activate()} {i + 1}");
+        }
+        for (int i = 0; i < range; i++)
+        {
+            Console.WriteLine("enter your choice");
+            int id = int.Parse(Console.ReadLine());
+            pinnedSensor(sensors[id - 1]);
+        }
 
-    
+        printList(Pinned);
+        return Pinned;
+    }
+
+
+    public  List<Sensor> pinnedSensor(Sensor s)
+    {
+        Pinned.Add(s);
+        return Pinned;
+    }
+
+    public Dictionary<string, int> convertListToDictionary(List<string> weakness)
+    {
+        Dictionary<string, int> weaknesDict = new Dictionary<string, int>();
+        for (int i = 0; i < weakness.Count; i++)
+        {
+            string key = weakness[i];
+
+            if (weaknesDict.ContainsKey(key))
+            {
+                weaknesDict[key]++; 
+            }
+            else
+            {
+                weaknesDict[key] = 1;
+            }
+        }
+
+        return weaknesDict;
+    }
+
+
+
+
 
     public List<string> createWeaknes(List<string> sensors,int range)
     {
@@ -30,5 +75,17 @@ public abstract class IranianAgent
             this.weaknes.Add(sensors[weak1]);
         }
         return this.weaknes;
+    }
+
+
+
+
+    //לניסוי בלבד
+    public static void printList(List<Sensor>l)
+    {
+        for(int i = 0; i < l.Count; i++)
+        {
+            Console.WriteLine(l[i].Activate());
+        }
     }
 }
