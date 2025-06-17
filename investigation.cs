734ed0fd22b8db5872a071a8f130a0d22e347d6b
elazar matan <elazarmata8@gmail.com>
2025-06-17 09:26:20 +0300

    Add invest method to Investigation class
    Introduced a new `invest` method in the `Investigation` class, which utilizes a list of sensors and interacts with an `IranianAgent` instance. Added a `using` directive for `System.Collections.Generic` to support the use of `List<T>`.

--- investigation.cs
+++ investigation.cs
@@ -1,4 +1,11 @@
-﻿public static class Investigation
-{
+﻿using System.Collections.Generic;
 
+public static  class Investigation
+{
+    public static void invest()
+    {
+        List<string> sensors = new List<string>() { "audio" };
+        IranianAgent age = new agentRank1("faruk");
+        List<string> weaknes = age.createWeaknes(sensors,2);
+    }
 }
