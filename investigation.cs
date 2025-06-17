c74506fbf19c667fe58c8c2f1f51fd90e6ba7cef
elazar matan <elazarmata8@gmail.com>
2025-06-17 16:18:09 +0300

    Refactor Investigation methods for agent handling
    - Updated `investRank1` to accept a single name parameter.
    - Added `createWeaknes` to generate weaknesses from sensors.
    - Introduced `convertListToDictionary` for counting weaknesses.
    - Modified `contains` to use the new dictionary for weakness checks.

--- investigation.cs
+++ investigation.cs
@@ -4,31 +4,67 @@ using System.Collections.Generic;
 public static  class Investigation
 {
     
-    public static IranianAgent investRank1(List<string> sensorname, List<Sensor> sensors)
+    public static IranianAgent investRank1(string name)
     {
-        IranianAgent age = new agentRank1("faruk");
-        List<string> weaknes = age.createWeaknes(sensorname,age.id);
-        age.f();
+        IranianAgent age = new agentRank1(name);
         return age;
     }
 
 
-    
-    
-
-    public static bool contains(IranianAgent agent)
+    public static List<string> createWeaknes(List<string> sensors, int range)
     {
-       
+        List<string> weaknes = new List<string>();
+        Random rand = new Random();
+        for (int i = 0; i < range; i++)
+        {
+            int weak1 = rand.Next(sensors.Count);
+            weaknes.Add(sensors[weak1]);
+        }
+        return weaknes;
+    }
 
+    public static Dictionary<string, int> convertListToDictionary(List<string> weakness)
+    {
+        Dictionary<string, int> weaknesDict = new Dictionary<string, int>();
+        for (int i = 0; i < weakness.Count; i++)
+        {
+            string key = weakness[i];
+
+            if (weaknesDict.ContainsKey(key))
+            {
+                weaknesDict[key]++;
+            }
+            else
+            {
+                weaknesDict[key] = 1;
+            }
+        }
+
+        foreach (var sens in weaknesDict)
+        {
+            Console.WriteLine($"{sens.Key}:{sens.Value}");
+        }
+
+
+        return weaknesDict;
+    }
+
+
+
+    public static bool contains(IranianAgent agent, List<string> weakness)
+    {
+
+
+            Dictionary<string, int> weaknesDict = convertListToDictionary(weakness);
         int required = agent.id;
         int count = 0;
         foreach (Sensor s in agent.Pinned) 
         {
-            if (agent.weaknesDict.ContainsKey(s.Activate()))
+            if (weaknesDict.ContainsKey(s.Activate()))
             {
-                if (agent.weaknesDict[s.Activate()] != 0)
+                if (weaknesDict[s.Activate()] != 0)
                 {
-                    agent.weaknesDict[s.Activate()] --;
+                    weaknesDict[s.Activate()] --;
                     count++;
                     Console.WriteLine($"succes {count}/{required}");
                     if(count == required)
