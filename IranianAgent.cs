1a4a7ec6e8828a2de0cc1f22da4f4fcd31c2897f
elazar matan <elazarmata8@gmail.com>
2025-06-17 16:17:09 +0300

    Refactor IranianAgent class for sensor management
    Updated the `weaknes` field to a `List<Sensor>` and removed the `weaknesDict` dictionary. Simplified the `getSensor` method to return `void` and streamlined the `pinnedSensor` method to only add sensors to the `Pinned` list. Removed `convertListToDictionary` and `createWeaknes` methods to improve clarity in weakness handling.

--- IranianAgent.cs
+++ IranianAgent.cs
@@ -6,9 +6,7 @@ public abstract class IranianAgent
     public string name;
     protected string rank;
     public int id;
-    public List<string> weaknes = new List<string>();
     public List<Sensor> Pinned = new List<Sensor>();
-    public Dictionary<string, int> weaknesDict;
 
     public IranianAgent(string name,string rank,int id)
     {
@@ -17,7 +15,7 @@ public abstract class IranianAgent
         this.id = id;
     }
 
-    public List<Sensor> getSensor(List<Sensor> sensors, int range)
+    public void getSensor(List<Sensor> sensors, int range)
     {
         for (int i = 0; i < sensors.Count; i++)
         {
@@ -28,63 +26,21 @@ public abstract class IranianAgent
             Console.WriteLine("enter your choice");
             int id = int.Parse(Console.ReadLine());
             pinnedSensor(sensors[id - 1]);
-        }
-
-        printList(Pinned);
-        return Pinned;
+        }     
     }
 
 
-    public  List<Sensor> pinnedSensor(Sensor s)
+    public  void pinnedSensor(Sensor s)
     {
-        Pinned.Add(s);
-        return Pinned;
+        Pinned.Add(s);     
     }
 
-    public Dictionary<string, int> convertListToDictionary(List<string> weakness)
-    {
-        Dictionary<string, int> weaknesDict = new Dictionary<string, int>();
-        for (int i = 0; i < weakness.Count; i++)
-        {
-            string key = weakness[i];
+    
 
-            if (weaknesDict.ContainsKey(key))
-            {
-                weaknesDict[key]++; 
-            }
-            else
-            {
-                weaknesDict[key] = 1;
-            }
-        }
-
-        foreach(var sens in weaknesDict)
-        {
-            Console.WriteLine($"{sens.Key}:{sens.Value}");
-        }
-        
-
-        return weaknesDict;
-    }
-
-    public void f()
-    {
-        this.weaknesDict = convertListToDictionary(weaknes);
-    }
+    
 
 
-
-    public List<string> createWeaknes(List<string> sensors,int range)
-    {
-        
-        Random rand = new Random();
-        for(int i = 0;i < range;i++)
-        {
-            int weak1 = rand.Next(sensors.Count);
-            this.weaknes.Add(sensors[weak1]);
-        }
-        return this.weaknes;
-    }
+    
 
 
 
