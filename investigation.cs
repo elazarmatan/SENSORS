b8dad1df5a6502577e258d05d9d4f69b6cdacba2
elazar matan <elazarmata8@gmail.com>
2025-06-17 12:36:05 +0300

    Add methods for IranianAgent weakness handling
    - Introduced `investRank1` method in `Investigation` class to create an `IranianAgent` and retrieve weaknesses based on sensor names.
    - Added `contains` method to check if an `IranianAgent` can be pinned based on its weaknesses, iterating through pinned sensors and managing weakness counts.
    - Updated using directives to include `System` and `System.Collections.Generic`.

--- investigation.cs
+++ investigation.cs
@@ -1,11 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public static  class Investigation
 {
-    public static void invest()
+    
+    public static IranianAgent investRank1(List<string> sensorname, List<Sensor> sensors)
     {
-        List<string> sensors = new List<string>() { "audio" };
         IranianAgent age = new agentRank1("faruk");
-        List<string> weaknes = age.createWeaknes(sensors,2);
+        List<string> weaknes = age.createWeaknes(sensorname,2);
+        
+        Dictionary<string, int> weaknesDict = age.convertListToDictionary(weaknes);
+        return age;
+    }
+
+
+    
+    
+
+    public static bool contains(IranianAgent agent)
+    {
+        int required = agent.id;
+        int count = 0;
+        foreach (Sensor s in agent.Pinned) 
+        {
+            if (agent.weaknesDict.ContainsKey(s.Activate()))
+            {
+                Console.WriteLine("a");
+                if (agent.weaknesDict[s.Activate()] != 0)
+                {
+                    Console.WriteLine("b");
+                    agent.weaknesDict[s.Activate()] --;
+                    count++;
+                    Console.WriteLine($"succes {count} / {required}");
+                    if(count == required)
+                    {
+                        Console.WriteLine("c");
+                        return true;
+                        
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
