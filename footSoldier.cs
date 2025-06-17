593ec6ef86b14beee1ea61568f9919ff23adeb08
elazar matan <elazarmata8@gmail.com>
2025-06-17 12:35:53 +0300

    Update agentRank1 constructor to include rank parameter
    Modified the constructor of the `agentRank1` class to accept an additional integer parameter. This change allows for a more detailed initialization of the agent's rank by passing the value `2` to the base class, enhancing the representation of the agent's rank.

--- footSoldier.cs
+++ footSoldier.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 
 public class agentRank1: IranianAgent
 {
-    public agentRank1(string name) : base(name, "agent Rank1") 
+    public agentRank1(string name) : base(name, "agent Rank1",2) 
     {
        
     }
 
+    
 }
