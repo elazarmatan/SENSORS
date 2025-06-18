aeff9083ba8704e7a79ed52d913adf9da5fa06bd
elazar matan <elazarmata8@gmail.com>
2025-06-18 17:37:04 +0300

    Refactor GameManager: Replace game method with menu
    The `GameManager` class has been updated to remove the `game` method and introduce a new `menu` method. This method generates a random room number and displays messages for four game levels, enhancing the user experience with color-coded output. It also includes calls to `Investigation.invest1()` for level 1 and `Investigation.invest("agentRank2")` for level 2.

--- gameManager.cs
+++ gameManager.cs
@@ -4,21 +4,42 @@ using System.Runtime.InteropServices;
 
 public static class GameManager
 {
-    public static void game()
-    {
-        Investigation.invest("agentRank1");
-    }
+    
 
 
 
     public static void menu()
     {
+        Random rand = new Random();
+        int room = rand.Next(1,10);
+        Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine(
             "=======================\n"+
             "      SENSORS GAME\n"+
             "=======================\n"
             );
+        Console.ResetColor();
 
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("LEVEL 1");
+        Console.WriteLine($"The agent continues in room {room}");
+        Console.ResetColor();
+        Investigation.invest1();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("LEVEL 2");
+        Console.WriteLine($"The agent continues in room {room}");
+        Console.ResetColor();
+        Investigation.invest("agentRank2");
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("LEVEL 3");
+        Console.WriteLine($"The agent continues in room {room}");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("LEVEL 4");
+        Console.WriteLine($"The agent continues in room {room}");
+        Console.ResetColor();
     }
 }
