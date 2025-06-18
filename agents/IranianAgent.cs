f6e99ff28978be3a3796913194bc5042f9619544
elazar matan <elazarmata8@gmail.com>
2025-06-18 12:17:48 +0300

    Add IranianAgent class and factory for agent creation
    - Implemented abstract class `IranianAgent` with properties for name, rank, sensor slots, and a list of pinned sensors.
    - Added methods for pinning sensors and printing sensor names.
    - Created static class `FactoryAgents` to manage agent types and instantiate agents using reflection.
    - Introduced `agentRank1` class inheriting from `IranianAgent` with a specific constructor.

---
+++ agents/IranianAgent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Lifetime;
+
+public abstract class IranianAgent
+{
+    public string name;
+    protected string rank;
+    public int sensorSlots;
+    public List<Sensor> Pinned = new List<Sensor>();
+
+    public IranianAgent(string name,string rank,int sensorSlots)
+    {
+        this.name = name;
+        this.rank = rank;
+        this.sensorSlots = sensorSlots;
+    }
+
+    public void pinnedSensor(List<string> sensors, int range)
+    {
+        for (int i = 0; i < sensors.Count; i++)
+        {
+            Console.WriteLine($"{sensors[i]} : {i + 1}");
+        }
+        for (int i = 0; i < range; i++)
+        {
+            Console.WriteLine("enter your choice");
+            int id = int.Parse(Console.ReadLine());
+            Pinned.Add(FactorySensor.createSensor(sensors[id - 1]));
+        }
+        printList(Pinned);
+    }
+
+    //לניסוי בלבד
+    public static void printList(List<Sensor>l)
+    {
+        for(int i = 0; i < l.Count; i++)
+        {
+            Console.WriteLine(l[i].name);
+        }
+    }
+}
