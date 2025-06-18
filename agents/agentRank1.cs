050568bc1df51766397684ed58326708a3393ba0
elazar matan <elazarmata8@gmail.com>
2025-06-18 17:28:55 +0300

    Add agentRank1 class inheriting from IranianAgent
    Introduces the `agentRank1` class with a constructor that sets the name to "agent Rank1" and a rank of 2. Includes an overridden `atack` method that currently returns an empty string.

---
+++ agents/agentRank1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class agentRank1 : IranianAgent
+{
+    public agentRank1() : base("agent Rank1",2) 
+    {
+       
+    }
+
+    public override string atack(IranianAgent agent)
+    {
+        return "";
+    }
+}
