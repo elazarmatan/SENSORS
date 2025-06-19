559f73154a09189c50a0c563ad33a441ee14240e
elazar matan <elazarmata8@gmail.com>
2025-06-19 09:44:04 +0300

    Add agentRank3 class with sensor removal functionality
    Introduces the `agentRank3` class inheriting from `IranianAgent`.
    Includes a constructor for initialization and overrides the `atack`
    method to randomly select and remove two sensors from the
    `Pinned` list, returning a message about the deleted sensors.

---
+++ agents/agentRank3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class agentRank3 : IranianAgent
+{
+    public agentRank3() : base("agentRank3", 4)
+    {
+
+    }
+
+    public override string atack(IranianAgent agent)
+    {
+        Random rand = new Random();
+        int idx1 = rand.Next(agent.Pinned.Count);
+        string sens1 = agent.Pinned[idx1].name;
+        agent.Pinned.RemoveAt(idx1);
+        int idx2 = rand.Next(agent.Pinned.Count);
+        string sens2 = agent.Pinned[idx2].name;
+        agent.Pinned.RemoveAt(idx2);
+        return $"the sensors :{sens1}  {sens2} deleted";
+    }
+}
